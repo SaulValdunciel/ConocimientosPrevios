7bad66072afef67dbdeb7b8d412e7903c5d1a4ac
SaulValdunciel <svalduncielg01@educantabria.es>
2026-09-22 20:05:12 +0200

    Cerre el Scaner

--- GestionDeTareas/src/main/java/gestiondetareas/GestorTareas.java
+++ GestionDeTareas/src/main/java/gestiondetareas/GestorTareas.java
@@ -4,6 +4,7 @@
  */
 package gestiondetareas;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,22 +13,28 @@ import java.util.List;
  */
 public class GestorTareas {
     
-    
+    List<String> tareas = new ArrayList<>();
     
     public void registrarTarea(String tarea){
-       
+       // for each para buscar si existe ya la tarea si es no existe crear y añadirla (add)
+       //for (String tareas :)
         
     }
     
     public void marcarTarea(String tarea){
-        
+        // for each para buscar si existe ya la tarea si es asi marcarla como completada
     }
     
     public void listarTareas(){
-        
+        //for each de la lista de las tareas e imprimir
+         
+        //for(String tareas : lista)
+            //System.out.println(tareas);
     }
     
     public void eliminarTareas(String tarea){
+        //for each para buscar si existe la tarea si es asi eliminarla (delete)
+        
         
     }
 }
